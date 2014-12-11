092237dbe2fe0a8dd3d6836da008a50e03648927
Darvas Gábor <darvas.gabor@soulware.hu>
2014-12-11 14:56:25 +0100

    basic refactoring to class

--- theme_merge.config.php
+++ theme_merge.config.php
@@ -1,15 +1,20 @@
 <?php
 
-if(!isset($merge_config)) $merge_config = array();
+namespace Soulware;
 
-$merge_config[] = array(
-	'sourcefile' => '_head.tpl',
-	'type' => 'template',
-	//not required, but exact match and only one result is expected
-	'tag' => '</head>',
-	//[append, prepend]
-	'insert_method' => 'prepend',
-	'content' => '<!--html code to insert-->',
-);
+class themeMergeConfig{
+    
+    public $sourcefile;
+    public $type = 'template';
+    //not required, but exact match and only one result is expected
+    public $tag;
+    //[append, prepend]
+    public $insert_method = 'prepend';
+    public $content;
 
-?>
+    public function __construct($sourcefile, $content) {
+        $this->sourcefile = $sourcefile;
+        $this->content = $content;
+    }
+    
+}
